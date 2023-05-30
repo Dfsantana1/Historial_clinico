237bfdef203b0442503a422170db89a892f24cd5
Dfsantana1 <122237882+Dfsantana1@users.noreply.github.com>
2023-05-30 15:15:09 -0500

    simplificacion_historial

--- historial.py
+++ historial.py
@@ -21,33 +21,3 @@ class HistorialClinico:
         self.enfermedades_actuales = enfermedades_actuales
         self.fecha_consulta = fecha_consulta
 
-
-class HistorialClinicoMayoresHombres(HistorialClinico):
-    def __init__(self, numero_documento, nombre, edad, altura, peso, telefono, email, direccion,
-                 sintomas, fecha_inicio_sintomas, quejas, alergias, medicamentos, enfermedades_hereditarias,
-                 enfermedades_actuales, fecha_consulta, enfermedades_transmision_sexual):
-        super().__init__(numero_documento, nombre, edad, "Hombre", altura, peso, telefono, email, direccion,
-                         sintomas, fecha_inicio_sintomas, quejas, alergias, medicamentos, enfermedades_hereditarias,
-                         enfermedades_actuales, fecha_consulta)
-        self.enfermedades_transmision_sexual = enfermedades_transmision_sexual
-
-class HistorialClinicoMayoresMujeres(HistorialClinico):
-    def __init__(self, numero_documento, nombre, edad, altura, peso, telefono, email, direccion,
-                 sintomas, fecha_inicio_sintomas, quejas, alergias, medicamentos, enfermedades_hereditarias,
-                 enfermedades_actuales, fecha_consulta, enfermedades_transmision_sexual, metodo_planificacion,
-                 estado_embarazo):
-        super().__init__(numero_documento, nombre, edad, "Mujer", altura, peso, telefono, email, direccion,
-                         sintomas, fecha_inicio_sintomas, quejas, alergias, medicamentos, enfermedades_hereditarias,
-                         enfermedades_actuales, fecha_consulta)
-        self.enfermedades_transmision_sexual = enfermedades_transmision_sexual
-        self.metodo_planificacion = metodo_planificacion
-        self.estado_embarazo = estado_embarazo
-
-class HistorialClinicoMenores(HistorialClinico):
-    def __init__(self, numero_documento, nombre, edad, altura, peso, telefono, email, direccion,
-                 sintomas, fecha_inicio_sintomas, quejas, alergias, medicamentos, enfermedades_hereditarias,
-                 enfermedades_actuales, fecha_consulta):
-        super().__init__(numero_documento, nombre, edad, "Menor", altura, peso, telefono, email, direccion,
-                         sintomas, fecha_inicio_sintomas, quejas, alergias, medicamentos, enfermedades_hereditarias,
-                         enfermedades_actuales, fecha_consulta)
-
